0fa28261d48da8a99b47bfa29aa20acad8943e57
Jean Oliveira <jeanoliveiraxiii@gmail.com>
2025-12-24 09:20:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,20 @@ def listar_cliente_especifico(id):
     
     return jsonify({'id': cliente.id, 'nome': cliente.nome})
 
+@app.route('/cliente/<int:id>/servicos', methods =['GET'])
+def listar_servicos_cliente(id):
+    cliente = Cliente.query.get(id)
+
+    if not cliente:
+        return jsonify({'erro': 'cliente não encontrado'}), 404
+    
+    servicos_cliente = []
+    for servico in cliente.servicos:
+        servicos_cliente.append({'id':servico.id,
+                                 'titulo': servico.titulo,
+                                 'descricao': servico.descricao})
+    return jsonify(servicos_cliente)
+
 @app.route('/clientes/<int:id>', methods = ['PUT'])
 def atualizar_cliente(id):
     cliente = Cliente.query.get(id)
@@ -82,15 +96,23 @@ def deletar_cliente(id):
 def criar_servico():
     dados = request.json
     titulo = dados['titulo']
-    descricao = dados['descricao']
+    descricao = dados.get('descricao')
+    cliente_id = dados['cliente_id']
 
-    servico = Servico(titulo=titulo, descricao = descricao)
+    cliente = Cliente.query.get(cliente_id)
+    if not cliente:
+        return jsonify({'erro': 'cliente não encontrado'}), 404
+
+    servico = Servico(titulo=titulo,
+                      descricao = descricao,
+                      cliente_id = cliente_id)
     db.session.add(servico)
     db.session.commit()
 
     return jsonify({'id': servico.id,
                     'titulo': servico.titulo,
-                    'descricao': servico.descricao})
+                    'descricao': servico.descricao,
+                    'cliente_id': servico.cliente_id})
 
 @app.route('/servicos', methods = ['GET'])
 def listar_servicos():
@@ -101,7 +123,9 @@ def listar_servicos():
     for servico in servicos:
         lista_servicos.append({'id': servico.id,
                                'titulo': servico.titulo,
-                               'descricao': servico.descricao})
+                               'descricao': servico.descricao,
+                               'cliente_id': servico.clieente_id,
+                               'cliente_nome': servico.cliente.nome})
     return jsonify(lista_servicos)
 
 @app.route('/servicos/<int:id>', methods = ['GET'])
